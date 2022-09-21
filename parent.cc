80709443eb84d104cd122ad70911213c86212d10
oscarsaavedrax <oscar.saavedra01@student.csulb.edu>
2022-09-20 17:58:13 -0700

    Parent process dynamically create n children with command line arguments

---
+++ parent.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <unistd.h>
+#include <sys/wait.h>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	// Create variables
+	int number_children = (argc - 1) / 2;
+	
+	//Print number of children
+	cout << "I have " << number_children << " children." << endl;
+	
+	//Print the aruments as gender-name pairs
+	for (int i = 1; i < argc; i += 2)
+		cout << argv[i] << " " << argv[i+1] << endl;
+	cout << endl;
+	
+	// Create n-number of child processes
+	for(int i = 0; i < number_children; i++)
+	{
+		if(fork() == 0)
+		{
+			cout << "son pid " << getpid() << endl;
+			exit(0);
+		}
+	}
+	// Parent process waits for children and terminates at the end
+	for(int i = 0; i < number_children; i++)
+		wait(NULL);
+	cout << "All child processes terminated. Parent exits." << endl;
+	return 0;
+}
