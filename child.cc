52eab21d4ca8170a3dd3902a19a15602d3fe675b
oscarsaavedrax <oscar.saavedra01@student.csulb.edu>
2022-09-21 18:41:59 -0700

    Print child process information

---
+++ child.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{	
+	//Print child number, gender, and name
+	cout << "Child # [number]" << ": I am a [gender], and my name is [name]." << endl;
+	return 0;
+}
